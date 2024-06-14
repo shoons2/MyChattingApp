174f402057b6922f7c60eec26258e6cf37d8bf17
shoons2 <brian021902@gmail.com>
2024-06-14 19:35:42 +0900

    0614

--- app/src/main/java/com/example/mychattingapp/MainActivity.java
+++ app/src/main/java/com/example/mychattingapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.mychattingapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnHost;
+    Button btnJoin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +23,24 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
+        });
+
+        btnHost = findViewById(R.id.btnHost);
+        btnJoin = findViewById(R.id.btnJoin);
+        btnHost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), HostActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnJoin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),ConnectActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
